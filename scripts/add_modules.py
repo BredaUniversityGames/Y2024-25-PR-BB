169f86eed3697392902d321974a45b9ea6d2f4b1
Santiago Bernardino <santibberna@gmail.com>
2024-09-11 14:26:04 +0200

    [ADD] all submodules

--- scripts/add_modules.py
+++ scripts/add_modules.py
@@ -9,7 +9,7 @@ def check_result(code):
 
 def add_submodule(git_link):
     folder_name = git_link[git_link.rfind("/")+1:git_link.rfind(".git")]
-    return subprocess.run(["git", "submodule", "add", git_link, folder_name]).returncode
+    return subprocess.run(["git", "submodule", "add", git_link, THIRD_PARTY_ROOT + folder_name]).returncode
 
 def main():
     for arg in sys.argv[1:]:
