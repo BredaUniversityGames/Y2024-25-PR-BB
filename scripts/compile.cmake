4b92445d83b274469803d856c772bf073185f33d
Santiago Bernardino <santibberna@gmail.com>
2024-09-18 09:48:26 +0200

    [Fix] presets

--- scripts/compile.cmake
+++ scripts/compile.cmake
@@ -6,10 +6,6 @@ set(CMAKE_SYSTEM_NAME Windows)
 # Specify the target architecture
 set(CMAKE_SYSTEM_PROCESSOR x64)
 
-# Specify the compiler paths
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
-
 set(CMAKE_CXX_FLAGS "-fexceptions -frtti -Wall -fuse-ld=lld")
 set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
