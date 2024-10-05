6a80e24304bb99154ec0387af4a6db4c5d6b3b93
Ferri de Lange <ev.fdl.ve@gmail.com>
2024-10-05 11:57:18 +0200

    [UPDATED] VMA batch file for improved time stamp

--- scripts/transfer_vma.bat
+++ scripts/transfer_vma.bat
@@ -1,31 +1,17 @@
 @echo off
 
-:: Get the current date (in YYYY-MM-DD format)
-for /f "tokens=2-4 delims=/.- " %%a in ('date /t') do (
-    set year=%%c
-    set month=%%a
-    set day=%%b
-)
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
-:: Get the current time (in HH-MM-SS format)
-for /f "tokens=1-3 delims=:., " %%a in ('echo %time%') do (
-    set hour=%%a
-    set minute=%%b
-    set second=%%c
-)
-
-:: Remove leading spaces from hour (if present)
-if %hour% lss 10 (set hour=0%hour%)
-
-:: Combine date and time into a single variable
-set datestamp=%year%-%month%-%day%_%hour%-%minute%-%second%
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
 :: Set variables for remote and local files
 set steamDeckIP=192.168.1.105
 set remoteFilePath=/home/deck/devkit-game/bb_game/vma_stats.json
 set outDir=../memory-vis
-set localFilePath=%outDir%/vma_stats_%datestamp%.json
-set outputMemVisPath=%outDir%/vma_stats_%datestamp%.png
+set localFilePath=%outDir%/vma_stats_%fullstamp%.json
+set outputMemVisPath=%outDir%/vma_stats_%fullstamp%.png
 
 if not exist "%outDir%" (
     mkdir "%outDir%"
