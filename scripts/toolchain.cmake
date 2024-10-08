32d197ceee7c17182c568694d369b29c32db15fb
Santiago Bernardino <santibberna@gmail.com>
2024-10-08 10:55:04 +0200

    [Add] Set static linking to windows only

--- scripts/toolchain.cmake
+++ scripts/toolchain.cmake
@@ -9,4 +9,7 @@ set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
 
-set(CMAKE_EXE_LINKER_FLAGS "-static")
+if (WIN32)
+    message(STATUS "### Using Static Linking for Compatibility")
+    set(CMAKE_EXE_LINKER_FLAGS "-static-libstdc++ -static-libgcc")
+endif ()
