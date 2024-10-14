d9771a4350541a850b3d214508f52b7f6cb27c6b
Marcin Zalewski <marcinzal24@gmail.com>
2024-10-14 16:45:25 +0200

    Sorting the graph

--- engine/source/frame_graph.cpp
+++ engine/source/frame_graph.cpp
@@ -7,14 +7,86 @@ FrameGraph::FrameGraph()
 
 void FrameGraph::Build()
 {
+    // First compute edges between nodes
     for (uint32_t i = 0; i < _nodes.size(); i++)
     {
-        if (_nodes[i].isEnabled)
+        if (!_nodes[i].isEnabled)
         {
             continue;
         }
 
         ComputeNodeEdges(_nodes[i], i);
+    }
+
+    // Now run the topological sort
+    enum class NodeStatus
+    {
+        eNotProcessed,
+        eVisited,
+        eAdded
+    };
+
+    std::vector<FrameGraphNodeHandle> reverseSortedNodes(_nodes.size());
+    std::vector<NodeStatus> nodesStatus(_nodes.size(), NodeStatus::eNotProcessed);
+    std::vector<FrameGraphNodeHandle> nodesToProcess{};
+    nodesToProcess.reserve(_nodes.size());
+
+    for (int i = 0; i < _nodes.size(); i++)
+    {
+        if (!_nodes[i].isEnabled)
+        {
+            continue;
+        }
+
+        nodesToProcess.push_back(i);
+
+        while(!nodesToProcess.empty())
+        {
+            const FrameGraphNodeHandle nodeHandle = nodesToProcess.back();
+
+            // Outputs might link to multiple nodes, and we don’t want to add the producing node multiple times to the sorted node list.
+            if (nodesStatus[nodeHandle] == NodeStatus::eAdded)
+            {
+                nodesToProcess.pop_back();
+                continue;
+            }
+
+            // If the node we are currently processing has already been visited, and we got to it in the stack,
+            // it means we processed all of its children, and it can be added to the list of sorted nodes.
+            if (nodesStatus[nodeHandle] == NodeStatus::eVisited)
+            {
+                nodesStatus[nodeHandle] == NodeStatus::eAdded;
+
+                reverseSortedNodes.push_back(nodeHandle);
+                nodesToProcess.pop_back();
+                continue;
+            }
+
+            nodesStatus[nodeHandle] = NodeStatus::eVisited;
+            const FrameGraphNode& node = _nodes[nodeHandle];
+
+            // If the node has no edges, it is a leaf node
+            if (node.edges.empty())
+            {
+                continue;
+            }
+
+            // If the node is not a leaf node, add its children for processing
+            for (const FrameGraphNodeHandle childNodeHandle : node.edges)
+            {
+                if(nodesStatus[childNodeHandle] != NodeStatus::eNotProcessed)
+                {
+                    nodesToProcess.push_back(childNodeHandle);
+                }
+            }
+        }
+    }
+
+    _sortedNodes.clear();
+
+    for (uint32_t i = reverseSortedNodes.size() - 1; i >= 0; --i)
+    {
+        _sortedNodes.push_back(reverseSortedNodes[i]);
     }
 }
 
@@ -76,6 +148,7 @@ FrameGraphResourceHandle FrameGraph::CreateOutputResource(const FrameGraphResour
     const FrameGraphResourceHandle resourceHandle = _resources.size();
     FrameGraphResource& resource = _resources.emplace_back();
     resource.type = creation.type;
+    resource.name = creation.name;
 
     if (creation.type != FrameGraphResourceType::eReference)
     {
@@ -96,7 +169,7 @@ FrameGraphResourceHandle FrameGraph::CreateInputResource(const FrameGraphResourc
     const FrameGraphResourceHandle resourceHandle = _resources.size();
     FrameGraphResource& resource = _resources.emplace_back();
     resource.type = creation.type;
-    resource.info = creation.info;
+    resource.name = creation.name;
 
     return resourceHandle;
 }
