30551b1cf30e68fa7e4124bf1527e5588a2db6df
Ferri de Lange <210166@buas.nl>
2024-10-14 16:07:32 +0200

    Feature/gpu culling (#51)
    * [ADDED] Culling compute shader and basic setup
    
    * [ADDED] Descriptor sets for the indirect draw call buffers in BatchBuffer.
    
    * [FIXED] Correctly creating the descriptor sets for the indirect draw buffer.
    
    * [ADDED] Implemented writing of compute shader to draw indirect buffer.
    
    * [ADDED] GPU culling implemented.
    
    * [ADDED] Support for indirect count
    
    * [UPDATED] Consistent variable naming in shader
    
    * [UPDATED] Moved draw commands buffer to GPU scene.
    
    * [UPDATED] Culler into it's own class
    [UPDATED] Camera structure into own class
    
    * [ADDED] Culler added to scene drawer passes.
    
    * [ADDED] Culling on shadow and geometry pass.
    
    * [UPDATED] Imgui.ini is now ignored.
    [ADDED] More editor fields for shadow camera.
    
    * [REVERT] .run file change
    
    * [UPDATE] Added dependency on original shader files for glsl compilation.
    
    * [FIXED] Removed unnecessary alignas specifier.

--- engine/include/pipelines/shadow_pipeline.hpp
+++ engine/include/pipelines/shadow_pipeline.hpp
@@ -3,6 +3,7 @@
 #include "gbuffers.hpp"
 #include "mesh.hpp"
 #include "geometry_pipeline.hpp"
+#include "indirect_culler.hpp"
 
 class BatchBuffer;
 class RenderSceneDescription;
@@ -13,17 +14,24 @@ public:
     ShadowPipeline(const VulkanBrain& brain, const GBuffers& gBuffers, const GPUScene& gpuScene);
     ~ShadowPipeline();
 
-    void RecordCommands(vk::CommandBuffer commandBuffer, uint32_t currentFrame, const RenderSceneDescription& scene, const BatchBuffer& batchBuffer);
+    void RecordCommands(vk::CommandBuffer commandBuffer, uint32_t currentFrame, const RenderSceneDescription& scene);
 
     NON_MOVABLE(ShadowPipeline);
     NON_COPYABLE(ShadowPipeline);
 
 private:
     void CreatePipeline(const GPUScene& gpuScene);
+    void CreateDrawBufferDescriptorSet(const GPUScene& gpuScene);
 
     const VulkanBrain& _brain;
     const GBuffers& _gBuffers;
 
+    CameraResource _shadowCamera;
+    IndirectCuller _culler;
+
     vk::PipelineLayout _pipelineLayout;
     vk::Pipeline _pipeline;
+
+    ResourceHandle<Buffer> _drawBuffer;
+    vk::DescriptorSet _drawBufferDescriptorSet;
 };
