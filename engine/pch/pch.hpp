3801a946946f5e8060d00b813f104cadc6cfbc50
Santiago Bernardino <santibberna@gmail.com>
2024-10-15 11:27:51 +0200

    [Add] PCH compile target

---
+++ engine/pch/pch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+// Includes commonly used std headers
+// TODO: Can include commonly used libraries here as well
+
+#include <iostream>
+#include <cstdint>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <thread>
+#include <memory>
+#include <optional>
+#include <functional>
+#include <chrono>
