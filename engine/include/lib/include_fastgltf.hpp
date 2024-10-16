f1d7726c8fa3534dd45494c4944e6f8578f3ba16
Santiago Bernardino <santibberna@gmail.com>
2024-10-16 16:00:03 +0200

    [Fix] Ignoring unknown pragmas

--- engine/include/lib/include_fastgltf.hpp
+++ engine/include/lib/include_fastgltf.hpp
@@ -4,7 +4,6 @@
 
 #pragma GCC diagnostic ignored "-Wredundant-move"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wunknown-pragmas"
 
 #include <fastgltf/core.hpp>
 #include <fastgltf/tools.hpp>
