b060b589875e57925fe91c4e8b08afb68d9880e6
Ferri de Lange <ev.fdl.ve@gmail.com>
2024-11-17 13:55:31 +0100

    [FIXED] Compilation error with unit tests

--- tests/resource_manager_tests.cpp
+++ tests/resource_manager_tests.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
 #include "resource_manager.hpp"
+
+#include <memory>
 #include <string>
 
 template <>
@@ -9,28 +11,28 @@ std::weak_ptr<ResourceManager<std::string>> ResourceHandle<std::string>::manager
 TEST(ResourceManagerTests, Creating)
 {
     // Arrange
-    ResourceManager<std::string> rm;
+    std::shared_ptr<ResourceManager<std::string>> rm { std::make_shared<ResourceManager<std::string>>() };
     std::string original = "my_resource";
     std::string resource = original;
 
     // Act
-    rm.Create(std::move(resource));
+    rm->Create(std::move(resource));
 
     // Assert
-    EXPECT_GT(rm.Resources().size(), 0);
-    EXPECT_EQ(rm.Resources().begin()->resource, original);
+    EXPECT_GT(rm->Resources().size(), 0);
+    EXPECT_EQ(rm->Resources().begin()->resource, original);
 }
 
 TEST(ResourceManagerTests, Accessing)
 {
     // Arrange
-    ResourceManager<std::string> rm;
+    std::shared_ptr<ResourceManager<std::string>> rm { std::make_shared<ResourceManager<std::string>>() };
     std::string original = "my_resource";
     std::string resource = original;
 
     // Act
-    auto handle = rm.Create(std::move(resource));
-    auto accessedResource = rm.Access(handle);
+    auto handle = rm->Create(std::move(resource));
+    auto accessedResource = rm->Access(handle);
 
     // Assert
     EXPECT_FALSE(accessedResource == nullptr);
@@ -40,35 +42,35 @@ TEST(ResourceManagerTests, Accessing)
 TEST(ResourceManagerTests, Destroying)
 {
     // Arrange
-    ResourceManager<std::string> rm;
+    std::shared_ptr<ResourceManager<std::string>> rm { std::make_shared<ResourceManager<std::string>>() };
     std::string original = "my_resource";
     std::string resource = original;
 
     // Act
-    auto handle = rm.Create(std::move(resource));
-    rm.Destroy(handle);
+    auto handle = rm->Create(std::move(resource));
+    rm->Destroy(handle);
 
     // Assert
-    EXPECT_EQ(rm.Access(handle), nullptr);
+    EXPECT_EQ(rm->Access(handle), nullptr);
 }
 
 TEST(ResourceManagerTests, Versioning)
 {
     // Arrange
-    ResourceManager<std::string> rm;
+    std::shared_ptr<ResourceManager<std::string>> rm { std::make_shared<ResourceManager<std::string>>() };
     std::string original1 = "my_resource1";
     std::string resource1 = original1;
     std::string original2 = "my_resource2";
     std::string resource2 = original2;
 
     // Act
-    auto handle1 = rm.Create(std::move(resource1));
-    rm.Destroy(handle1);
+    auto handle1 = rm->Create(std::move(resource1));
+    rm->Destroy(handle1);
 
-    auto handle2 = rm.Create(std::move(resource2));
+    auto handle2 = rm->Create(std::move(resource2));
 
     // Assert
-    EXPECT_EQ(rm.Access(handle1), nullptr);
-    EXPECT_EQ(*rm.Access(handle2), original2);
-    EXPECT_EQ(rm.Resources().size(), 1);
+    EXPECT_EQ(rm->Access(handle1), nullptr);
+    EXPECT_EQ(*rm->Access(handle2), original2);
+    EXPECT_EQ(rm->Resources().size(), 1);
 }
