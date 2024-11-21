88a7cf366334ed2eddefb949bbb5f62b75f73c33
luuk234 <224822@buas.nl>
2024-11-21 20:56:31 +0100

    Update mesh_resource_manager.hpp

--- engine/renderer/public/resource_management/mesh_resource_manager.hpp
+++ engine/renderer/public/resource_management/mesh_resource_manager.hpp
@@ -1,16 +1,33 @@
 #pragma once
 
+#include "batch_buffer.hpp"
+
 #include <memory>
 #include <vulkan/vulkan.hpp>
 
 #include "common.hpp"
-#include "resource_manager.hpp"
+#include "model.hpp"
 
+class BatchBuffer;
+class SingleTimeCommands;
+
+#include "resource_manager.hpp"
 class VulkanContext;
 struct Mesh;
 
 class MeshResourceManager final : public ResourceManager<Mesh>
 {
 public:
-    MeshResourceManager();
+    MeshResourceManager() = default;
+    ~MeshResourceManager() = default;
+
+    // deferred init
+    void SetBatchBuffer(std::shared_ptr<BatchBuffer> batchBuffer) { _batchBuffer = batchBuffer; };
+
+    ResourceHandle<Mesh> Create(SingleTimeCommands& commandBuffer, const CPUResources::Mesh& data, std::vector<ResourceHandle<Material>> materials = {});
+    ResourceHandle<Mesh> Create(SingleTimeCommands& commandBuffer, const CPUResources::Mesh::Primitive& data, ResourceHandle<Material> material);
+
+private:
+    std::shared_ptr<BatchBuffer> _batchBuffer;
+    Mesh::Primitive CreatePrimitive(SingleTimeCommands& commandBuffer, const CPUResources::Mesh::Primitive& data, ResourceHandle<Material> material);
 };
