15b9ee5c66086b9085594c83c237609b2289b223
LeoHellmig <220383@buas.nl>
2024-11-25 18:21:31 +0100

    DebugCallback for logging in bblog

--- engine/audio/private/audio_module.cpp
+++ engine/audio/private/audio_module.cpp
@@ -1,21 +1,61 @@
 #include "audio_module.hpp"
 
+#include <string>
+
 #include "fmod.h"
 #include "fmod_common.h"
 #include "fmod_errors.h"
 #include "log.hpp"
 
+#if not defined(NDEBUG)
+FMOD_RESULT DebugCallback(FMOD_DEBUG_FLAGS flags, MAYBE_UNUSED const char* file, int line, const char* func, const char* message)
+{
+    // Get rid of "\n" for better formatting in bblog
+    std::string msg(message);
+    msg.pop_back();
+
+    switch (flags)
+    {
+    case FMOD_DEBUG_LEVEL_LOG:
+        bblog::info("FMOD : {0} ( {1} ) - {2}", line, func, msg);
+        break;
+    case FMOD_DEBUG_LEVEL_WARNING:
+        bblog::warn("FMOD : {0} ( {1} ) - {2}", line, func, msg);
+        break;
+    case FMOD_DEBUG_LEVEL_ERROR:
+        bblog::error("FMOD : {0} ( {1} ) - {2}", line, func, msg);
+        break;
+    default:
+        break;
+    }
+
+    return FMOD_OK;
+}
+#endif
+
 ModuleTickOrder AudioModule::Init(MAYBE_UNUSED Engine& engine)
 {
     const ModuleTickOrder tickOrder = ModuleTickOrder::ePostTick;
 
-    FMOD_RESULT result = FMOD_System_Create(&_fmodSystem, FMOD_VERSION);
+    FMOD_RESULT result;
+
+    result = FMOD_System_Create(&_fmodSystem, FMOD_VERSION);
 
     if (result != FMOD_OK)
     {
         bblog::error("FMOD Error: {0}", FMOD_ErrorString(result));
         return tickOrder;
     }
+
+#if not defined(NDEBUG)
+    // Use FMOD_DEBUG_LEVEL_MEMORY if you want to debug memory issues related to fmod
+    result = FMOD_Debug_Initialize(FMOD_DEBUG_LEVEL_LOG, FMOD_DEBUG_MODE_CALLBACK, &DebugCallback, nullptr);
+
+    if (result != FMOD_OK)
+    {
+        bblog::error("FMOD Error: {0}", FMOD_ErrorString(result));
+    }
+#endif
 
     result = FMOD_System_Init(_fmodSystem, 512, FMOD_INIT_NORMAL, nullptr);
 
