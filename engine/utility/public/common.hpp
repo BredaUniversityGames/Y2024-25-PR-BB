f3c230cbaa1d65ae1fe5be1265ddf4e4e8265f2e
Santiago Bernardino <santibberna@gmail.com>
2024-11-25 10:00:02 +0100

    [Fix] Macro mistake

--- engine/utility/public/common.hpp
+++ engine/utility/public/common.hpp
@@ -2,13 +2,15 @@
 
 // Class Decorations
 
-#define NON_COPYABLE(ClassName)              \
-    (ClassName)(const(ClassName)&) = delete; \
-    (ClassName)& operator=(const(ClassName)&) = delete;
+// NOLINTBEGIN
+#define NON_COPYABLE(ClassName)           \
+    ClassName(const ClassName&) = delete; \
+    ClassName& operator=(const ClassName&) = delete;
 
-#define NON_MOVABLE(ClassName)           \
-    (ClassName)((ClassName)&&) = delete; \
-    (ClassName)& operator=((ClassName)&&) = delete;
+#define NON_MOVABLE(ClassName)       \
+    ClassName(ClassName&&) = delete; \
+    ClassName& operator=(ClassName&&) = delete;
+// NOLINTEND
 
 // Attribute macros
 
