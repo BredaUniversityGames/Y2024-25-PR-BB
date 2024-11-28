49a078c77bc7367336d7c55d8d339ff3995e301b
luuk234 <224822@buas.nl>
2024-11-28 13:16:35 +0100

    added back menu test

--- engine/user_interface/private/ui_main_menu.cpp
+++ engine/user_interface/private/ui_main_menu.cpp
@@ -10,5 +10,47 @@ MainMenuCanvas::MainMenuCanvas(const glm::vec2& size, MAYBE_UNUSED std::shared_p
     : Canvas(size)
 {
 
-    // main menu will be created here in the next pr.
+    // demo scene
+
+    CPUImage buttonNormalImage;
+
+    buttonNormalImage.format
+        = vk::Format::eR8G8B8A8Unorm;
+    buttonNormalImage.SetFlags(vk::ImageUsageFlagBits::eSampled);
+    buttonNormalImage.ExtractDataFromPNG("assets/textures/button_background.png");
+    buttonNormalImage.isHDR = false;
+    auto normalImage = context->Resources()->ImageResourceManager().Create(buttonNormalImage);
+    auto hoveredImage = context->Resources()->ImageResourceManager().Create(buttonNormalImage);
+
+    UIButton::ButtonStyle standardStyle {
+        .normalImage = normalImage,
+        .hoveredImage = hoveredImage,
+        .pressedImage = normalImage
+    };
+
+    context->UpdateBindlessSet();
+
+    std::unique_ptr<UIButton> subButton = std::make_unique<UIButton>();
+    subButton->style = standardStyle;
+    subButton->SetScale({ 300, 100 });
+    subButton->SetLocation({ 100, 300 });
+    subButton->onMouseDownCallBack = []() {};
+    subButton->onBeginHoverCallBack = []() {};
+
+    AddChild(std::move(subButton));
+
+    std::unique_ptr<UIButton> playButton = std::make_unique<UIButton>();
+    playButton->style = standardStyle;
+    playButton->SetScale({ 300, 100 });
+    playButton->SetLocation({ 100, 100 });
+    playButton->onBeginHoverCallBack = []() {};
+
+    std::unique_ptr<UITextElement> playText = std::make_unique<UITextElement>(font);
+    playText->text = "Play the Game";
+    playText->SetScale({ 1, 1 });
+    playText->SetLocation({ 810, 640 });
+    playText->color = { 0, 1, 0, 1 };
+
+    AddChild(std::move(playText));
+    AddChild(std::move(playButton));
 }
