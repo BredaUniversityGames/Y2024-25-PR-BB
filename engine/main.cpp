4f2185df72ba3c149e17b3e5062230f242f0c55f
Marcin Zalewski <marcinzal24@gmail.com>
2024-11-28 11:52:46 +0100

    Feature/steam api (#84)
    * [ADDED] Steam API headers
    
    * [ADDED] Steam module
    
    * [ADDED] Steam app ID file
    
    * [ADDED] Steam API initialization
    
    * [UPDATE] Error messages
    
    * [UPDATE] Formatting
    
    * [FIX] Added missing include
    
    * [FIX] Packaging scripts now copy over needed steam files

--- engine/main.cpp
+++ engine/main.cpp
@@ -4,12 +4,14 @@
 #include "old_engine.hpp"
 #include "physics_module.hpp"
 #include "renderer_module.hpp"
+#include "steam_module.hpp"
 
 int main(MAYBE_UNUSED int argc, MAYBE_UNUSED char* argv[])
 {
     MainEngine instance;
 
     instance
+        .AddModule<SteamModule>()
         .AddModule<ApplicationModule>()
         .AddModule<OldEngine>()
         .AddModule<RendererModule>()
--- scripts/package-rel-deb.bat
+++ scripts/package-rel-deb.bat
@@ -1,3 +1,3 @@
 cd ..
-python scripts/python/package.py -o "package" -e "build/WSL-RelWithDebInfo/Game" -a "assets" "shaders/bin"
+python scripts/python/package.py -o "package" -e "build/WSL-RelWithDebInfo/Game" "steam_appid.txt" "external/steamworks/lib/Linux/libsteam_api.so" -a "assets" "shaders/bin"
 pause
