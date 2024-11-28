85187d5583722ecc9ff9752c391438b57f08ac50
Marcin Zalewski <marcinzal24@gmail.com>
2024-11-28 09:55:23 +0100

    [ADDED] Steam API initialization

--- engine/steam/private/steam_module.cpp
+++ engine/steam/private/steam_module.cpp
@@ -1,19 +1,57 @@
 #include "steam_module.hpp"
 #include <steam_api.h>
 
+extern "C" void __cdecl DebugCallback(int severity, const char* message)
+{
+    // if you're running in the debugger, only warnings (nSeverity >= 1) will be sent
+    // if you add -debug_steamapi to the command-line, a lot of extra informational messages will also be sent
+
+    switch (severity)
+    {
+    case 0:
+        bblog::info("[Steamworks] ", message);
+        break;
+    case 1:
+        bblog::warn("[Steamworks] ", message);
+        break;
+    default:
+        bblog::error("[Steamworks] ", message);
+    }
+}
+
 ModuleTickOrder SteamModule::Init(MAYBE_UNUSED Engine& engine)
 {
-    if(!SteamAPI_Init())
+    SteamErrMsg errorMessage = { 0 };
+    if (SteamAPI_InitEx(&errorMessage) != k_ESteamAPIInitResult_OK)
     {
-        assert("steam not avaiable!");
+        bblog::error("[Steamworks] ", errorMessage);
+        bblog::warn("[Steamworks] Steam is not running, Steamworks API functionality will be unavailable");
+
+        return ModuleTickOrder::ePreTick;
     }
+
+    SteamClient()->SetWarningMessageHook(&DebugCallback);
+
+    if (!SteamUser()->BLoggedOn())
+    {
+        bblog::warn("[Steamworks] Steam user is not logged in, Steamworks API functionality will be unavailable");
+
+        return ModuleTickOrder::ePreTick;
+    }
+
+    _steamAvailable = true;
 
     return ModuleTickOrder::ePreTick;
 }
 
 void SteamModule::Tick(MAYBE_UNUSED Engine& engine)
 {
+    if (!_steamAvailable)
+    {
+        return;
+    }
 
+    SteamAPI_RunCallbacks();
 }
 
 void SteamModule::Shutdown(MAYBE_UNUSED Engine& engine)
--- engine/steam/public/steam_module.hpp
+++ engine/steam/public/steam_module.hpp
@@ -9,10 +9,16 @@ class SteamModule : public ModuleInterface
     void Tick(MAYBE_UNUSED Engine& engine) override;
     void Shutdown(MAYBE_UNUSED Engine& engine) override;
 
+    bool _steamAvailable = false;
+
 public:
     NON_COPYABLE(SteamModule);
     NON_MOVABLE(SteamModule);
 
     SteamModule() = default;
     ~SteamModule() override = default;
+
+    // When the user launched the application through Steam, this will return true.
+    // If false, the steam module cannot be used, as Steam API does not work.
+    bool Available() const { return _steamAvailable; }
 };
