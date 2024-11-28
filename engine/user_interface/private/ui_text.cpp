607cdc1826d2977e339c0123c50911c296be83e3
luuk234 <224822@buas.nl>
2024-11-28 14:15:07 +0100

    Update ui_text.cpp

--- engine/user_interface/private/ui_text.cpp
+++ engine/user_interface/private/ui_text.cpp
@@ -6,14 +6,7 @@
 
 void UITextElement::SubmitDrawInfo(std::vector<QuadDrawInfo>& drawList) const
 {
-
     float localOffset = 0;
-
-    glm::vec2 scale;
-    if (GetScale().x == 0)
-        scale = glm::vec2(GetScale().y);
-    else
-        scale = GetScale();
 
     for (const auto& i : text)
     {
@@ -31,7 +24,7 @@ void UITextElement::SubmitDrawInfo(std::vector<QuadDrawInfo>& drawList) const
 
             glm::vec2 normalizedBearing = glm::vec2(character.Bearing) / static_cast<float>(_font->characterHeight);
             glm::vec3 translation = glm::vec3(GetAbsouluteLocation() + glm::vec2(localOffset + normalizedBearing.x, normalizedBearing.y), 0);
-            glm::vec3 localScale = glm::vec3(glm::vec2(character.Size) / glm::vec2(_font->characterHeight), 1.0) * glm::vec3(scale, 0);
+            glm::vec3 localScale = glm::vec3(glm::vec2(character.Size) / glm::vec2(_font->characterHeight), 1.0) * glm::vec3(GetScale(), 0);
 
             info.modelMatrix = (glm::scale(glm::translate(glm::mat4(1), translation), localScale));
             info.textureIndex = _font->_fontAtlas.Index();
@@ -41,11 +34,11 @@ void UITextElement::SubmitDrawInfo(std::vector<QuadDrawInfo>& drawList) const
 
             drawList.emplace_back(info);
 
-            localOffset += (float(character.Advance >> 6) / _font->characterHeight) * scale.x; // Convert advance from 1/64th pixels
+            localOffset += (float(character.Advance >> 6) / _font->characterHeight) * GetScale().x; // Convert advance from 1/64th pixels
         }
         else
         {
-            localOffset += (static_cast<int>(scale.x)) / 4.0;
+            localOffset += (static_cast<int>(GetScale().x)) / 4.0;
         }
     }
 }
