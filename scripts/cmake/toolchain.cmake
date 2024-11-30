5408403f75daa1cb869db08b162c9be596d64c4b
Santiago Bernardino <santibberna@gmail.com>
2024-11-30 11:37:35 +0100

    [Changed] toolchain now uses clang [Fix] duplicated sections

--- scripts/cmake/toolchain.cmake
+++ scripts/cmake/toolchain.cmake
@@ -1,14 +1,16 @@
+SET(CMAKE_C_COMPILER "clang")
+SET(CMAKE_C_FLAGS "-std=c99")
+SET(CMAKE_C_FLAGS_DEBUG "-g")
+SET(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+SET(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG")
+SET(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
 
-# Use GCC to compile the project (by default, CLion uses GCC)
-# you will have to set in your cmake command
+SET(CMAKE_CXX_COMPILER "clang++")
+SET(CMAKE_CXX_FLAGS "-std=c++20")
+SET(CMAKE_CXX_FLAGS_DEBUG "-g")
+SET(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+SET(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
+SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
 
-# this file only specifies global compilation flags
-
-set(CMAKE_CXX_FLAGS "-fexceptions -frtti -fmax-errors=0")
-set(CMAKE_CXX_FLAGS_DEBUG "-g -O0")
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
-
-if (WIN32)
-    set(CMAKE_EXE_LINKER_FLAGS "-static")
-endif ()
+SET(CMAKE_AR "llvm-ar")
+SET(CMAKE_LINKER "lld")
