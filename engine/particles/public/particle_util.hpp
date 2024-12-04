5a7519f983ecd0f688e409711ad7f8ca89956a24
lyvanettinger <223052@buas.nl>
2024-12-04 10:58:27 +0000

    Feature/billboard particle orientation (#95)
    * [ADDED] Rotating Jeremi billboards

--- engine/particles/public/particle_util.hpp
+++ engine/particles/public/particle_util.hpp
@@ -5,7 +5,7 @@
 
 // temporary values for testing/progress
 static constexpr uint32_t MAX_EMITTERS = 32;
-static constexpr int32_t MAX_PARTICLES = 1024;
+static constexpr int32_t MAX_PARTICLES = 1024 * 256;
 
 // Structs in line with shaders
 struct alignas(16) Emitter
@@ -14,11 +14,11 @@ struct alignas(16) Emitter
     uint32_t count = 0;
     glm::vec3 velocity = { 0.0f, 0.0f, 0.0f };
     float mass = 0.0f;
-    glm::vec3 rotationVelocity = { 0.0f, 0.0f, 0.0f };
+    glm::vec2 rotationVelocity = { 0.0f, 0.0f };
     float maxLife = 1.0f;
     float randomValue = 0.0f;
-    uint32_t materialIndex = 0;
     glm::vec2 size = { 1.0f, 1.0f };
+    uint32_t materialIndex = 0;
 };
 
 struct alignas(16) Particle
@@ -27,7 +27,7 @@ struct alignas(16) Particle
     float mass = 0.0f;
     glm::vec3 velocity = { 0.0f, 0.0f, 0.0f };
     float maxLife = 5.0f;
-    glm::vec3 rotationVelocity = { 0.0f, 0.0f, 0.0f };
+    glm::vec2 rotationVelocity = { 0.0f, 0.0f };
     float life = 5.0f;
     uint32_t materialIndex = 0;
     glm::vec2 size = { 1.0f, 1.0f };
@@ -45,6 +45,7 @@ struct alignas(16) ParticleInstance
     glm::vec3 position = { 0.0f, 0.0f, 0.0f };
     uint32_t materialIndex = 0;
     glm::vec2 size = { 1.0f, 1.0f };
+    float angle = 0.0f;
 };
 
 struct alignas(16) CulledInstances
