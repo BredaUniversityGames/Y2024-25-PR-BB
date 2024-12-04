ef95389dce150cb9332907c555c0dcd8a49b6389
Santiago Bernardino <santibberna@gmail.com>
2024-12-04 09:45:56 +0100

    Feature/wren bindings (#85)
    * [WIP] Wren Entry point and Foreign helpers
    
    * [Removed] Wren C library in favour of an extend C++ one
    
    * [Removed] test script bloat [Changed] Now working with the new library
    
    * [Add] Time module [Add] basic foreign data structure tests
    
    * [Add] Foreign API for reflecting engine modules
    
    * [Add] improved error handling in MainScript [Add] Comments [Fix] Missing headers
    
    * [Fix] merge issues
    
    * [Fix] Tracy policy error
    
    * [Fix] PR checks wrong name define
    
    * [Fix] Cannot use lambdas in binding api :(
    
    * [Add] Fork of wrenbind with import handling [Changed] simplified main script and scripting context logic
    
    * [Changed] Engine instance is now passed to wren scripts instead of being a global
    
    * [Fix] unit test bugs [Add] engine_api.wren to .gitignore
    
    * [Fix] linter warnings

---
+++ engine/scripting/public/utility/wren_engine.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "engine.hpp"
+#include "wren_common.hpp"
+
+// Wrapper class for Accessing the engine
+struct WrenEngine
+{
+    Engine* instance {};
+
+    template <typename T>
+    std::optional<T*> GetModule()
+    {
+        if (instance == nullptr)
+            return std::nullopt;
+
+        if (auto ptr = instance->GetModuleSafe<T>())
+            return ptr;
+
+        return std::nullopt;
+    }
+};
