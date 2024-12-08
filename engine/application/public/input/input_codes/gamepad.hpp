21863304be49f71c3632b6bce857c0867226ff8f
Marcin Zalewski <marcinzal24@gmail.com>
2024-12-08 11:53:38 +0100

    [CHANGE] Removed unnecessary gamepad enum prefixes

--- engine/application/public/input/input_codes/gamepad.hpp
+++ engine/application/public/input/input_codes/gamepad.hpp
@@ -5,46 +5,47 @@
 
 enum class GamepadButton : uint32_t
 {
-    eGAMEPAD_BUTTON_SOUTH, /* Bottom face button (e.g. Xbox A button) */
-    eGAMEPAD_BUTTON_EAST, /* Right face button (e.g. Xbox B button) */
-    eGAMEPAD_BUTTON_WEST, /* Left face button (e.g. Xbox X button) */
-    eGAMEPAD_BUTTON_NORTH, /* Top face button (e.g. Xbox Y button) */
-    eGAMEPAD_BUTTON_BACK,
-    eGAMEPAD_BUTTON_GUIDE,
-    eGAMEPAD_BUTTON_START,
-    eGAMEPAD_BUTTON_LEFT_STICK,
-    eGAMEPAD_BUTTON_RIGHT_STICK,
-    eGAMEPAD_BUTTON_LEFT_SHOULDER,
-    eGAMEPAD_BUTTON_RIGHT_SHOULDER,
-    eGAMEPAD_BUTTON_DPAD_UP,
-    eGAMEPAD_BUTTON_DPAD_DOWN,
-    eGAMEPAD_BUTTON_DPAD_LEFT,
-    eGAMEPAD_BUTTON_DPAD_RIGHT,
-    eGAMEPAD_BUTTON_MISC1, /* Additional button (e.g. Xbox Series X share button, PS5 microphone button, Nintendo Switch Pro capture button, Amazon Luna microphone button, Google Stadia capture button) */
-    eGAMEPAD_BUTTON_RIGHT_PADDLE1, /* Upper or primary paddle, under your right hand (e.g. Xbox Elite paddle P1) */
-    eGAMEPAD_BUTTON_LEFT_PADDLE1, /* Upper or primary paddle, under your left hand (e.g. Xbox Elite paddle P3) */
-    eGAMEPAD_BUTTON_RIGHT_PADDLE2, /* Lower or secondary paddle, under your right hand (e.g. Xbox Elite paddle P2) */
-    eGAMEPAD_BUTTON_LEFT_PADDLE2, /* Lower or secondary paddle, under your left hand (e.g. Xbox Elite paddle P4) */
-    eGAMEPAD_BUTTON_TOUCHPAD, /* PS4/PS5 touchpad button */
-    eGAMEPAD_BUTTON_MISC2, /* Additional button */
-    eGAMEPAD_BUTTON_MISC3, /* Additional button */
-    eGAMEPAD_BUTTON_MISC4, /* Additional button */
-    eGAMEPAD_BUTTON_MISC5, /* Additional button */
-    eGAMEPAD_BUTTON_MISC6, /* Additional button */
+    eSOUTH, /* Bottom face button (e.g. Xbox A button) */
+    eEAST, /* Right face button (e.g. Xbox B button) */
+    eWEST, /* Left face button (e.g. Xbox X button) */
+    eNORTH, /* Top face button (e.g. Xbox Y button) */
+    eBACK,
+    eGUIDE,
+    eSTART,
+    eLEFT_STICK,
+    eRIGHT_STICK,
+    eLEFT_SHOULDER,
+    eRIGHT_SHOULDER,
+    eDPAD_UP,
+    eDPAD_DOWN,
+    eDPAD_LEFT,
+    eDPAD_RIGHT,
+    eMISC1, /* Additional button (e.g. Xbox Series X share button, PS5 microphone button, Nintendo Switch Pro capture button, Amazon Luna microphone button, Google Stadia capture button) */
+    eRIGHT_PADDLE1, /* Upper or primary paddle, under your right hand (e.g. Xbox Elite paddle P1) */
+    eLEFT_PADDLE1, /* Upper or primary paddle, under your left hand (e.g. Xbox Elite paddle P3) */
+    eRIGHT_PADDLE2, /* Lower or secondary paddle, under your right hand (e.g. Xbox Elite paddle P2) */
+    eLEFT_PADDLE2, /* Lower or secondary paddle, under your left hand (e.g. Xbox Elite paddle P4) */
+    eTOUCHPAD, /* PS4/PS5 touchpad button */
+    eMISC2, /* Additional button */
+    eMISC3, /* Additional button */
+    eMISC4, /* Additional button */
+    eMISC5, /* Additional button */
+    eMISC6, /* Additional button */
 };
 
 enum class GamepadAxis : uint32_t
 {
-    eGAMEPAD_AXIS_LEFTX,
-    eGAMEPAD_AXIS_LEFTY,
-    eGAMEPAD_AXIS_RIGHTX,
-    eGAMEPAD_AXIS_RIGHTY,
-    eGAMEPAD_AXIS_LEFT_TRIGGER,
-    eGAMEPAD_AXIS_RIGHT_TRIGGER,
+    eLEFTX,
+    eLEFTY,
+    eRIGHTX,
+    eRIGHTY,
+    eLEFT_TRIGGER,
+    eRIGHT_TRIGGER,
 };
 
 enum class GamepadAnalog : uint32_t
 {
-    eGAMEPAD_AXIS_LEFT,
-    eGAMEPAD_AXIS_RIGHT,
+    eAXIS_LEFT,
+    eAXIS_RIGHT,
+    eDPAD,
 };
