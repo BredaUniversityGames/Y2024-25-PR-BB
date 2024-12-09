ca90735066fcbc018bb0a62d98612591fae515e1
Santiago Bernardino <santibberna@gmail.com>
2024-12-09 16:30:37 +0100

    Feature/ecs bindings (#100)
    * [Add] basic wrappers for components and entities in wren
    
    * [Add] bindings module
    
    * [Add] Complete bindings for working with transform component
    
    * [Add] Destroy entity bindings
    
    * [Fix] unused warning

---
+++ engine/bindings/private/utility/wren_entity.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <entt/entity/registry.hpp>
+#include <optional>
+
+template <typename T>
+struct WrenComponent;
+
+struct WrenEntity
+{
+    entt::entity entity;
+    entt::registry* registry;
+
+    template <typename T>
+    std::optional<WrenComponent<T>> GetComponent();
+
+    template <typename T>
+    WrenComponent<T> AddComponent();
+};
+
+template <typename T>
+struct WrenComponent
+{
+    WrenEntity entity {};
+    T* component {};
+};
+
+template <typename T>
+WrenComponent<T> WrenEntity::AddComponent()
+{
+    registry->emplace_or_replace<T>(entity);
+    return WrenComponent<T> { WrenEntity { entity, registry }, &registry->get<T>(entity) };
+};
+
+template <typename T>
+std::optional<WrenComponent<T>> WrenEntity::GetComponent()
+{
+    if (auto* out = registry->try_get<T>(entity))
+    {
+        return WrenComponent<T> { WrenEntity { entity, registry }, out };
+    }
+    return std::nullopt;
+};
