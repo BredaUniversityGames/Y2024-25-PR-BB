67f878937f674cedfd9e66fbcb9b91dd8d05f965
LeoHellmig <220383@buas.nl>
2024-12-17 10:08:11 +0100

    Update audio_emitter_component.hpp

--- engine/audio/public/audio_emitter_component.hpp
+++ engine/audio/public/audio_emitter_component.hpp
@@ -3,6 +3,9 @@
 #include "audio_common.hpp"
 #include "imgui_entt_entity_editor.hpp"
 
+// Add audio to this component by emplace_back(PlaySound/PlayEvent)
+// Do not add the same sound to multiple emitters, it will sound weird
+// SoundInstances and EvenInstances are automatically removed if they aren't looping
 struct AudioEmitterComponent
 {
     std::vector<SoundInstance> _soundIds {};
