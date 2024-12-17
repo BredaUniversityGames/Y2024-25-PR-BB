48dba69369d43ab354806a0c2b9b5d5e700abe75
Marcin Zalewski <marcinzal24@gmail.com>
2024-12-17 09:43:21 +0100

    [CHANGE] Added comment for setting the build live

--- scripts/build_system/package_and_upload_steam_public.bat
+++ scripts/build_system/package_and_upload_steam_public.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Uploading to the public branch.
+echo Uploading to the public branch. Setting the build live is not done automatically for public builds, that must be done via the admin panel!
 set /p username="Steam username: "
 set /p password="Steam password: "
 steam\builder\steamcmd.exe +login %username% %password% +run_app_build ..\config\app_build_public.vdf
