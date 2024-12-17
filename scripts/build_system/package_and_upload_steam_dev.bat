6e3b13e05b9f35a7d155164d29390861735d877f
Marcin Zalewski <marcinzal24@gmail.com>
2024-12-17 11:17:56 +0100

    [FIX] Packaging build before uploading to Steam

--- scripts/build_system/package_and_upload_steam_dev.bat
+++ scripts/build_system/package_and_upload_steam_dev.bat
@@ -1,4 +1,6 @@
 @echo off
+call package_dist_linux.bat
+call package_dist_windows.bat
 set /p username="Steam username: "
 set /p password="Steam password: "
 steam\builder\steamcmd.exe +login %username% %password% +run_app_build ..\config\app_build_3365920.vdf
