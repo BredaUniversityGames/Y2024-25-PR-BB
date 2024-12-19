eff52a49456eb040b6e7be2c88eb2e544a7f53af
LeoHellmig <220383@buas.nl>
2024-12-19 13:52:41 +0100

    Feature/audio wren bindings (#118)
    * include fmod lib for windows
    
    This does not build, there seems to be an issue with the fmod.lib being incompatible with mingw
    
    * update fmod and switch to C API
    
    Turns out FMOD does not support MinGW for the C++ API
    
    * Copy dlls to ouput directory
    
    * Update audio_module
    
    * proper shutdown
    
    * cleanup
    
    * Adding correct dlls
    
    * DebugCallback for logging in bblog
    
    * remove crash on shutdown
    
    * Loading and playing sound
    
    * Adding fmod studio system
    
    * Update CMakeLists.txt
    
    * Loading banks and playing events
    
    * Playback functionality, testing, reorganizing
    
    * rename
    
    * Update audio_module.cpp
    
    * Cleaning up and adding comments
    
    * Update audio_module.cpp
    
    * includes
    
    * Update audio_module.hpp
    
    * cpp-linter
    
    * Update engine.cpp
    
    * Adding foundation for audio system and components
    
    * Audio system groundworks
    
    * some updates
    
    * Improved API, added sound instances
    
    * Update audio_module.cpp
    
    * Debug info for audio
    
    * Better Inspection
    
    * refactoring and 3d sounds
    
    * include moved
    
    * remove popping sound from eagle screech
    
    * 3D audio, audio debug lines
    
    * cleanup and tracy
    
    * 3D events and debug lines on sounds
    
    * Renamed isplaying, check for transforms separately when updating the audio system
    
    * Listener component is now empty
    
    * Audio listener now "inspectable"
    
    * Update audio_listener_component.cpp
    
    * Update audio_emitter_component.hpp
    
    * Update audio_system.cpp
    
    * adding includes
    
    * linter and pr checks
    
    * Update audio_listener_component.cpp
    
    * Update audio_system.cpp
    
    * Update audio_system.cpp
    
    * Update audio_common.hpp
    
    * fix errrors implement some feedback
    
    * fix errors?
    
    * use IDs for handles instead of soundInfo reference
    
    * Update engine.cpp
    
    * store soundinfo by value instead of reference
    
    * First audio wren bindings, allow non one-shot events to be played as one-shot
    
    * Wren bindings for audio + test in scripts
    
    * Update game.wren
    
    * Add release trigger action to steam input
    
    * Remove event playing test from c++
    
    * add includes
    
    * Update audio_bindings.cpp
    
    * only single shot for now, removed new steam action

--- engine/audio/public/audio_module.hpp
+++ engine/audio/public/audio_module.hpp
@@ -26,6 +26,9 @@ public:
     // Load sound, mp3 or .wav etc
     SoundID LoadSFX(SoundInfo& soundInfo);
 
+    // Checks if the sound is loaded
+    bool isSFXLoaded(std::string_view path) const;
+
     // Return the soundinfo associated with the path
     SoundID GetSFX(std::string_view path);
 
@@ -33,11 +36,16 @@ public:
     // PlaySound(...) is already used by a MinGW macro 💀
     SoundInstance PlaySFX(SoundID, float volume, bool startPaused);
 
+    // Set paused or unpaused
+    void SetPaused(SoundInstance instance, bool paused);
+
+    // Set variables can be added in the future if needed
+
     // Stops looping sounds
     // Regular sounds will stop by themselves once they are done
     void StopSFX(SoundInstance instance);
 
-    bool IsSoundPlaying(SoundInstance instance);
+    bool IsSFXPlaying(SoundInstance instance);
 
     // Load a .bank file
     // make sure to load the master bank and .strings.bank as well
