3adb652c336bbf0a80111c63e066c78cccbb474d
luuk234 <224822@buas.nl>
2025-01-08 15:38:56 +0100

    Update ui_menus.hpp

--- engine/user_interface/public/ui_menus.hpp
+++ engine/user_interface/public/ui_menus.hpp
@@ -1,6 +1,5 @@
 #pragma once
-#include "canvas.hpp"
-
+class Canvas;
 class GraphicsContext;
 
 std::unique_ptr<Canvas> CreateNavigationTestMenu(UIInputContext& uiInputContext, const glm::ivec2& canvasBounds, std::shared_ptr<GraphicsContext> graphicsContext);
