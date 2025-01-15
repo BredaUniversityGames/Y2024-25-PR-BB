e2f632fda40ced3fd7ca88e7357ebd63c1459cd5
LeoHellmig <220383@buas.nl>
2025-01-15 10:41:29 +0100

    Shooting (#136)
    * Wren binding for ShootRay function, function in use in scripts for shooting
    
    * Added game module, lifetime system, lifetime component, bindings for lifetime component, added particle emitter on player shot impact
    
    * Add mix to math bindings, add bullet trails by spawning emitters between the start and end point of a ray
    
    * Update game.wren
    
    * tidying
    
    * implementing feedback

---
+++ engine/game/public/game_module.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "common.hpp"
+#include "engine.hpp"
+
+class GameModule : public ModuleInterface
+{
+    ModuleTickOrder Init(Engine& engine) override;
+    void Tick(MAYBE_UNUSED Engine& engine) override;
+    void Shutdown(MAYBE_UNUSED Engine& engine) override;
+    std::string_view GetName() override { return "Game Module"; }
+
+public:
+    GameModule() = default;
+    ~GameModule() override = default;
+
+    NON_COPYABLE(GameModule);
+    NON_MOVABLE(GameModule);
+};
--- engine/inspector/private/editor.cpp
+++ engine/inspector/private/editor.cpp
@@ -17,6 +17,7 @@
 #include "gbuffers.hpp"
 #include "graphics_context.hpp"
 #include "imgui_backend.hpp"
+#include "lifetime_component.hpp"
 #include "log.hpp"
 #include "menus/performance_tracker.hpp"
 #include "model_loader.hpp"
@@ -48,6 +49,7 @@ Editor::Editor(ECSModule& ecs)
     _entityEditor.registerComponent<AudioEmitterComponent>("Audio Emitter");
     _entityEditor.registerComponent<AudioListenerComponent>("Audio Listener");
     _entityEditor.registerComponent<EmitterComponent>("Particle Emitter");
+    _entityEditor.registerComponent<LifetimeComponent>("Lifetime");
 }
 
 void Editor::DrawHierarchy()
