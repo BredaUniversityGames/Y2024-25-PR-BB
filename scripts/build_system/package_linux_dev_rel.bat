b81abf497e551bf2a7c00f133b43865c4cf90d79
Marcin Zalewski <marcinzal24@gmail.com>
2025-02-19 09:53:35 +0100

    [UPDATE] Added quotation marks in packaging scripts

--- scripts/build_system/package_linux_dev_rel.bat
+++ scripts/build_system/package_linux_dev_rel.bat
@@ -1,5 +1,5 @@
 cd ../..
-python scripts/python/package.py -c scripts/build_system/package_config/linux_dev_rel.json
+python scripts/python/package.py -c "scripts/build_system/package_config/linux_dev_rel.json"
 cd scripts/build_system
 echo Linux release for development build packaged.
 pause
--- scripts/build_system/package_linux_dist.bat
+++ scripts/build_system/package_linux_dist.bat
@@ -1,4 +1,4 @@
 cd ../..
-python scripts/python/package.py -c scripts/build_system/package_config/linux_dist.json
+python scripts/python/package.py -c "scripts/build_system/package_config/linux_dist.json"
 cd scripts/build_system
 echo Linux distribution build packaged.
