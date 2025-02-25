1b30802142af9feafec1543065eff94152381b44
Ferri de Lange <210166@buas.nl>
2025-02-25 15:16:06 +0100

    Feature/animation hierarchy optimization (#172)
    * [UPDATED] Scene changed to be better for testing animatinos
    
    * [UPDATE] Create profiling scene
    
    * [UPDATE] Linus fix
    
    * WIP animation changes
    
    * [ADDED] New skeleton hierarchy
    
    * [UPDATE] Use of new animation hierarchy
    
    * [UPDATE] Added profiling
    
    * [UPDATE] Increase test scene
    
    * [UPDATE] Better handling of matrices
    
    * [UPDATE] Update components to work correctly
    
    * [UPDATE] Resolve compile errors
    
    * [UPDATE] Resolve compile errors
    
    * [UPDATE] Resolve compile errors
    
    * [UPDATE] Resolve compile errors
    
    * [UPDATED] PR feedback
    
    * [UPDATED] Fixed issue with debug assert
    
    * [UPDATED] Revert settings
    
    * [UPDATED] Fix compile
    
    * [UPDATED] Updaets

--- engine/renderer/public/components/skeleton_component.hpp
+++ engine/renderer/public/components/skeleton_component.hpp
@@ -1,8 +1,35 @@
 #pragma once
 
 #include <cstdint>
+#include <entt/entt.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/mat4x4.hpp>
+
+struct SkeletonNodeComponent
+{
+    entt::entity parent { entt::null };
+    std::array<entt::entity, 7> children;
+};
 
 struct SkeletonComponent
 {
-    uint32_t boneOffset = 0;
+    entt::entity root { entt::null };
 };
+
+struct JointWorldTransformComponent
+{
+    glm::mat4 world { glm::identity<glm::mat4>() };
+};
+
+struct JointSkinDataComponent
+{
+    glm::mat4 inverseBindMatrix {};
+    uint32_t jointIndex {};
+    entt::entity skeletonEntity { entt::null };
+};
+
+namespace SkeletonHelpers
+{
+void AttachChild(entt::registry& registry, entt::entity parent, entt::entity child);
+void InitializeSkeletonNode(SkeletonNodeComponent& node);
+}
