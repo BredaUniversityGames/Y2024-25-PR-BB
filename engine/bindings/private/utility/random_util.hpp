69dab542ec98c5553c3ba51eb0710c79dd60c723
LarsJanssen21 <220015@buas.nl>
2025-02-25 13:17:56 +0100

    Removed redundant comment

--- engine/bindings/private/utility/random_util.hpp
+++ engine/bindings/private/utility/random_util.hpp
@@ -13,7 +13,7 @@ public:
     {
         std::random_device dev;
         std::mt19937 rng(dev());
-        std::uniform_int_distribution<std::mt19937::result_type> dist6(0, std::numeric_limits<uint32_t>::max()); // distribution in range [1, 6]
+        std::uniform_int_distribution<std::mt19937::result_type> dist6(0, std::numeric_limits<uint32_t>::max());
 
         return dist6(rng);
     }
