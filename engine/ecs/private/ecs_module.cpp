02aa0cb101d3b54f063a39830915ba0460265528
VengarvdWal <222790@buas.nl>
2025-02-26 14:52:47 +0100

    Fixed the Deletion of Skeletal Meshes
    Also should fix the CI-CD build

--- engine/ecs/private/ecs_module.cpp
+++ engine/ecs/private/ecs_module.cpp
@@ -1,7 +1,10 @@
 #include "ecs_module.hpp"
+
 #include "components/name_component.hpp"
+#include "components/relationship_component.hpp"
 #include "components/relationship_helpers.hpp"
 #include "components/transform_helpers.hpp"
+#include <components/skeleton_component.hpp>
 #include "scripting_module.hpp"
 #include "systems/physics_system.hpp"
 #include "time_module.hpp"
@@ -75,6 +78,18 @@ void ECSModule::DestroyEntity(entt::entity entity)
     assert(registry.valid(entity));
     registry.emplace_or_replace<DeleteTag>(entity);
     RelationshipComponent* relationship = registry.try_get<RelationshipComponent>(entity);
+    SkeletonNodeComponent* skeleton = registry.try_get<SkeletonNodeComponent>(entity);
+
+    if (skeleton != nullptr)
+    {
+        for (const auto& child : skeleton->children)
+        {
+            if (child != entt::null)
+            {
+                DestroyEntity(child);
+            }
+        }
+    }
     if (relationship != nullptr)
     {
         if (relationship->childrenCount > 0)
