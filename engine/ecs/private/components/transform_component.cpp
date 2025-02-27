9fd6279050684c4304e04f4a2dd154e09efff966
Ferri de Lange <ev.fdl.ve@gmail.com>
2025-02-27 16:42:29 +0100

    [UPDATED] Added euler rotation in editor

--- engine/ecs/private/components/transform_component.cpp
+++ engine/ecs/private/components/transform_component.cpp
@@ -1,6 +1,9 @@
 #include "components/transform_component.hpp"
 #include "components/transform_helpers.hpp"
 
+#include <glm/gtx/quaternion.hpp>
+#include <glm/vec4.hpp>
+
 namespace EnttEditor
 {
 template <>
@@ -13,9 +16,18 @@ void ComponentEditorWidget<TransformComponent>(entt::registry& reg, entt::regist
 void TransformComponent::Inspect(entt::registry& reg, entt::entity entity)
 {
     bool changed = false;
-    // TODO use euler angles for rotation
+
     changed |= ImGui::DragFloat3("Position##Transform", &_localPosition[0], 0.1f);
-    changed |= ImGui::DragFloat4("Rotation##Transform", &_localRotation[0], 0.1f);
+
+    glm::vec3 eulerAngles = glm::degrees(glm::eulerAngles(glm::quat(_localRotation)));
+
+    changed |= ImGui::DragFloat3("Rotation##Transform (Euler)", &eulerAngles[0], 0.1f);
+
+    if (changed)
+    {
+        _localRotation = glm::quat(glm::radians(eulerAngles));
+    }
+
     changed |= ImGui::DragFloat3("Scale##Transform", &_localScale[0], 0.1f);
 
     if (changed)
