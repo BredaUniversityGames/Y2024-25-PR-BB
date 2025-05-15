01c94022cd48aedc8fa7759c909e18ddcc0e6356
Lon van Ettinger <lonvanettinger@gmail.com>
2025-05-15 14:19:07 +0200

    [FIX] PR Checks error

--- engine/particles/private/particle_module.cpp
+++ engine/particles/private/particle_module.cpp
@@ -1,5 +1,3 @@
-#include "resource_management/image_resource_manager.hpp"
-
 #include "particle_module.hpp"
 
 #include "components/rigidbody_component.hpp"
@@ -13,6 +11,7 @@
 #include "physics_module.hpp"
 #include "renderer.hpp"
 #include "renderer_module.hpp"
+#include "resource_management/image_resource_manager.hpp"
 #include "time_module.hpp"
 
 #include <filesystem>
