ea907c294c294b432e10804ddca9d6c46b7c1bf5
Luuk Asmus <224822@buas.nl>
2025-06-05 13:55:01 +0200

    Feature/direction damage indicator (#301)
    * added damage indicator
    
    * player now takes damage again
    
    * added missing files
    
    * when restarting game damage indicator now not visible anymore

--- engine/user_interface/private/ui_image.cpp
+++ engine/user_interface/private/ui_image.cpp
@@ -7,9 +7,10 @@ void UIImage::SubmitDrawInfo(std::vector<QuadDrawInfo>& drawList) const
 {
     if (visibility == VisibilityState::eUpdatedAndVisible || visibility == VisibilityState::eNotUpdatedAndVisible)
     {
-        glm::mat4 matrix = glm::translate(glm::mat4(1), glm::vec3(GetAbsoluteLocation(), 0));
-        matrix = glm::scale(matrix, glm::vec3(GetAbsoluteScale(), 0));
-
+        glm::mat4 matrix = GetPreTransformationMatrix();
+        matrix = glm::translate(glm::mat4(matrix), glm::vec3(GetAbsoluteLocation(), 0));
+        matrix = glm::scale(matrix, glm::vec3(GetAbsoluteScale(), 0));;
+        
         QuadDrawInfo info {
             .matrix = matrix,
             .color = display_color,
